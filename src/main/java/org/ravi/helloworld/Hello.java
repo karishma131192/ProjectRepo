db9a952def1dafb26bdfd90f7aec896f09ab2aa0
PDC3A-Training.PDC3A <PDC3A-Training.PDC3A@accenture.com>
2018-04-06 15:59:45 +0530

    Changes to Index.jsp page

--- src/main/java/org/ravi/helloworld/Hello.java
+++ src/main/java/org/ravi/helloworld/Hello.java
@@ -2,14 +2,19 @@ package org.ravi.helloworld;
 
 public class Hello {
 	
+	
+	public static void main(String[] args)
+	{
+		int target = -5;
+		int num = 3;
+
+		target =- num;  // Noncompliant; target = -3. Is that really what's meant?
+		target =+ num;
+	}
+	
 	public String displayHelloMessage(String message){
 		message = "hi";
 		return message;
 	}
-	int target = -5;
-	int num = 3;
-
-	target =- num;  // Noncompliant; target = -3. Is that really what's meant?
-	target =+ num;
 
 }
